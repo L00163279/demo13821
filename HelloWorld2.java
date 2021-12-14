8395818b1ca2a3bdf04b5bdf4cc509a9bc13a425
MShafeeq <L00163279@student.lyit.ie>
2021-12-14 22:39:48 +0000

    commit

---
+++ HelloWorld2.java
@@ -0,0 +1,15 @@
+package mvn_hello2;
+
+public class HelloWorld2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(getGreetingMessage("Muhammed Shafeeq"));
+	}
+	
+	public static String getGreetingMessage(String name) {
+		return "Hello Mr "+ name + "! Welcome to LYIT";
+
+	}
+
+}
